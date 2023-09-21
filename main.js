e56f250e90d3388bdc3d6e9d0458e9a029e90561
Diego2907 <dverduzco10@ucol.mx>
2023-09-20 18:10:58 -0600

    primera version

---
+++ main.js
@@ -0,0 +1,66 @@
+var gato
+let carro
+console.log (gato)
+console.log (carro)
+
+var gato="Pelusa";
+console.log (gato)
+var gato="Tobyy";
+console.log (gato)
+
+let perro="Tobby";
+// let perro="Pelusa";
+console.log (perro);
+
+perro="bobby";
+console.log (perro);
+
+// const direccion
+const direccion = "Calle madero";
+console.log (direccion);
+
+direccion= 'calle torres'
+console.log (direccion);
+
+let $car= 'ford';
+console.log ($car);
+
+let UserName = 'Pedro';
+console.log (UserName);
+
+// let velocidad = 98.5;
+// console.log(typeof velocidad)
+
+// let UserName = 'Sergio';
+// console.log (typeof UserName)
+
+// let isActive = true;
+// console.log (typeof isActive);
+
+// let nombre;
+// console.log (typeof nombre);
+
+// let edad = null;
+// console.log (typeof edad);
+
+// let z = 3n ** 55n;
+// console.log (typeof z);
+
+// let Clave = Symbol('Sym');
+// console.log (typeof Clave);
+
+// let carros = ['ford', 500, true];
+// console.log (typeof (carros))
+
+// let carro = { modelo: 'ford', potencia: 300, velocidad: 200};
+// console.log (typeof carro);
+
+// let x = [2 , 4];
+// let y = x;
+
+// y.push(5);
+
+// console.log(x);
+// console.log(y);
+
+
